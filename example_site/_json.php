1dc8f73154b6e1bb41a7fbb8908c7ff8869139bb
Sean Bourg <sean.bourg@gmail.com>
2018-10-16 21:12:20 -0500

    Incremental change to json dispatcher - added file loader and payload return.

--- example_site/_json.php
+++ example_site/_json.php
@@ -11,24 +11,40 @@
 
 
 try{
+    
+    $GLOBALS['._import_path'] = '';
+    $GLOBALS['._view_path'] = __DIR__;
+    $GLOBALS['._view_path'] = "{$GLOBALS['._view_path']}\_content\json\%s.inc";
 
     /* Parse request */
     $_request_data = $_GET;
     $_request_path = $_request_data['__execution_path'] ?? '';
     unset($_request_data['__execution_path']);
 
+    // Get view file
+    $_execution_view = sprintf($GLOBALS['._view_path'], $_request_path);
+    $_json_data = ['hasError' => false, 'payload' => null ];
 
     // Check for file:
+    if ( false == file_exists($_execution_view) ){
+        http_response_code(404);
+        $_json_data['hasError'] = true;
+        $_json_data['payload'] = 'Unable to find requested resource';
+    }else{
+        $_json_data['payload'] = require($_execution_view);
+    }
 
-
-    // 404
 }catch(Exception $_exception){
-   // 500  
+    http_response_code(500);
+    $_json_data['hasError'] = true;
+    $_json_data['payload'] = $_exception->getMessage();
 }catch(Error $_exception){
-    // 500  
+    http_response_code(500);
+    $_json_data['hasError'] = true;
+    $_json_data['payload'] = $_exception->getMessage();
 }
 
-echo $_request_path;
-
-
+header('Content-Type: application/json', true);
+echo json_encode($_json_data);
+exit();
 
