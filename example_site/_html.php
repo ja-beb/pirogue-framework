38260f5613d9b466b29210c39d248f9f80e0b3b4
Sean Bourg <sean.bourg@gmail.com>
2018-10-16 20:49:17 -0500

    Added project structure and base (empty) files.

---
+++ example_site/_html.php
@@ -0,0 +1,6 @@
+<?php 
+
+    /**
+     * Main dispatcher for HTML content.
+     * @author Bourg, Sean P. <sean.bourg@gmail.com>
+     */
---
+++ example_site/_json.php
@@ -0,0 +1,6 @@
+<?php
+
+/**
+ * Main dispatcher for JSON content.
+ * @author Bourg, Sean P. <sean.bourg@gmail.com>
+ */
