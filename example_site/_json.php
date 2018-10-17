b27d8caa6c80e58abcae770e8dcc3d2f06fbd219
Sean Bourg <sean.bourg@gmail.com>
2018-10-16 20:56:36 -0500

    Update php project settings

--- example_site/_json.php
+++ example_site/_json.php
@@ -4,3 +4,31 @@
  * Main dispatcher for JSON content.
  * @author Bourg, Sean P. <sean.bourg@gmail.com>
  */
+
+/* Bootsrap */
+
+/* Declare page varaiables */
+
+
+try{
+
+    /* Parse request */
+    $_request_data = $_GET;
+    $_request_path = $_request_data['__execution_path'] ?? '';
+    unset($_request_data['__execution_path']);
+
+
+    // Check for file:
+
+
+    // 404
+}catch(Exception $_exception){
+   // 500  
+}catch(Error $_exception){
+    // 500  
+}
+
+echo $_request_path;
+
+
+
