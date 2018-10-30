a45e7f2d5140ce1ea8d76e6ba3c157c7c2bd6a8a
Sean Bourg <sean.bourg@gmail.com>
2018-10-29 21:16:20 -0500

    json build

--- example_site/_json.php
+++ example_site/_json.php
@@ -1,56 +1,65 @@
 <?php
+use function sprout\database_collection_open;
 
 /**
  * Main dispatcher for JSON content.
+ *
  * @author Bourg, Sean P. <sean.bourg@gmail.com>
  */
 
+// Send request headers (type for this dispatcher):
+header('Content-Type: application/json', true);
 
-function _json_send($data, bool $hasError){
-    header('Content-Type: application/json', true);
-    sprout\dispatcher_send( json_encode([
-        'hasError' => $hasError,
-        'payload' => $data
-    ]));
+// Load & intialize sprout framework:
+require '_include/sprout/import.inc';
+__import(realpath('_include'));
+
+// Import required libraries
+import('sprout/dispatcher');
+import('sprout/error_handler');
+import('sprout/json');
+import('sprout/database_collection');
+
+set_error_handler('sprout\error_handler');
+
+/**
+ * 
+ * @param ErrorException | Error $exception
+ */
+function _json_error($exception)
+{
+    http_response_code(500);
+    sprout\error_handler_log(sprout\database_collection_open('website'), $exception->getMessage(), $exception->getFile(), $exception->getLine());
+    sprout\dispatcher_send(json_encode($exception->getMessage()));
 }
 
 
-require '../sprout/dispatcher.inc';
 
-try{
-    
-    /* Declare page varaiables */
-    $GLOBALS['._import_path'] = '';
-    $GLOBALS['._view_path'] = __DIR__;
-    $GLOBALS['._view_path'] = "{$GLOBALS['._view_path']}\_json\%s.inc";
+try {
+    /* Initialize */
+    sprout\__database_collection(realpath('_config'));
 
     /* Parse request */
     $_request_data = $_GET;
     $_request_path = $_request_data['__execution_path'] ?? '';
     unset($_request_data['__execution_path']);
+    $_parts = explode('/', $_request_path);
+    $_module = array_shift($_parts);
 
-    // Get view file
-    $_execution_view = sprintf($GLOBALS['._view_path'], $_request_path);
-    $_json_data = ['hasError' => false, 'payload' => null ];
-
-    if ( false == file_exists($_execution_view) ){
-        // File not found error
-        http_response_code(404);
-        _json_send( "Unable to find requested resource: {$_request_path}", true);
-    }else{
-        // Load requested file
-        _json_send( require $_execution_view, false );
+    $_file = "_json/{$_module}.inc";
+    if (file_exists($_file)) {
+        require "_json/{$_module}.inc";
+        $_func = sprintf('%s\%s_%s', $_module, $_SERVER['REQUEST_METHOD'], array_shift($_parts));
+        if (function_exists($_func)) {
+            return sprout\dispatcher_send(json_encode($_func($_request_path, $_request_data, ('POST' == $_SERVER['REQUEST_METHOD']) ? $_POST : [])));
+        }
     }
 
-}catch(Exception $_exception){
-    http_response_code(500);
-    _json_send( $_exception->getMessage(), true );
-}catch(Error $_exception){
-    http_response_code(500);
-    _json_send( $_exception->getMessage(), true );
+    http_response_code(404);
+    sprout\dispatcher_set_cache_control(CACHE_CONTROL_TYPE_PRIVATE, - 1);
+    return sprout\dispatcher_send(json_encode('Unable to find requested item.'));
+} catch (Exception $_exception) {
+    return _json_error($_exception);
+} catch (Error $_exception) {
+    return _json_error($_exception);
 }
-
-header('Content-Type: application/json', true);
-echo json_encode($_json_data);
-exit();
-
