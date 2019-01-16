75bf15e6482b7deb3810eb5ec6a659d3b0f594aa
Sean Bourg <sean.bourg@gmail.com>
2019-01-15 19:21:55 -0600

    Moved json functions to sprout/json library file

--- example_site/_json.php
+++ example_site/_json.php
@@ -1,8 +1,9 @@
 <?php
+
 use function sprout\__import;
 use function sprout\import;
 use function sprout\database_collection_open;
-use function sprout\error_handler;
+use function sprout\_error_handler;
 use function sprout\_dispatcher_send;
 use function sprout\__database_collection;
 use function sprout\dispatcher_set_cache_control;
@@ -31,81 +32,18 @@ use function sprout\dispatcher_set_cache_control;
 // Send request headers (type for this dispatcher):
 header('Content-Type: application/json', true);
 
-// Declare base folders:
-define('_BASE_PATH', __DIR__);
-define('_BASE_PATH_CONTROLLER', sprintf('%s\_json', _BASE_PATH));
-
 // Load & intialize sprout framework:
 require '_include/sprout/import.inc';
-__import(sprintf('%s\_include', _BASE_PATH));
+__import(sprintf('%s\_include', __DIR__));
 
 // Import required libraries
 import('sprout/dispatcher');
 import('sprout/error_handler');
 import('sprout/database_collection');
+import('sprout/json');
 
-set_error_handler(function (string $message, string $file, int $line) {
-    error_handler($message, $file, $line);
-});
+set_error_handler('_error_handler');
 
-/**
- * Send json error message to user.
- *
- * @param
- *            ErrorException | Error $exception
- * @return string
- */
-function _json_error($exception): string
-{
-    http_response_code(500);
-    error_handler_log(database_collection_open('website'), $exception->getMessage(), $exception->getFile(), $exception->getLine());
-    return $exception->getMessage();
-}
-
-/**
- * JSON request not found (404).
- * string $path Requested resource.
- *
- * @return string
- */
-function _json_not_found(string $path): string
-{
-    http_response_code(404);
-    dispatcher_set_cache_control(CACHE_CONTROL_DISABLED);
-    return "Unable to find requested module: '{$path}.";
-}
-
-/**
- * Routes incoming request and return json encoded results.
- *
- * @param string $method
- * @param string $route
- * @param array $request_data
- * @param array $form_data
- * @return string
- */
-function _json_route(string $method, string $route, array $request_data, array $form_data): string
-{
-    try {
-        $_parts = explode('/', $route);
-        $_module = array_shift($_parts);
-
-        $_file = "_json/{$_module}.inc";
-        if (file_exists($_file)) {
-            require $_file;
-            $_func = sprintf('%s\%s_%s', $_module, $method, array_shift($_parts));
-            $_results = function_exists($_func) ? call_user_func($_func, implode('/', $_parts), $request_data, $form_data) : _json_not_found($route);
-        } else {
-            $_results = _json_not_found($route);
-        }
-    } catch (Exception $_exception) {
-        $_results = _json_error($_exception);
-    } catch (Error $_exception) {
-        $_results = _json_error($_exception);
-    }
-
-    return json_encode($_results);
-}
 
 /* Initialize */
 __database_collection(realpath('_config'));
