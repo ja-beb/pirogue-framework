f542133de8f04c3805053e627c2670d3952425c2
Sean Bourg <sean.bourg@gmail.com>
2019-03-19 11:49:27 -0500

    database creation and population

---
+++ example-site/sql/database-dump.ps1
@@ -0,0 +1,5 @@
+﻿$connection = Open-SqlConnection -Server 'invlabsserver' -Database 'example-site' -Username 'website_user' -Password '1coldheart'
+Get-SQLData -Connection $connection -Query "SELECT TABLE_SCHEMA, TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE='BASE TABLE'" | %{
+    $name = "{0}.{1}" -f $_.TABLE_SCHEMA, $_.TABLE_NAME
+    Get-SQLData -Connection $connection -Query ( "SELECT * from {0}" -f $name ) | export-csv -NoTypeInformation ("C:\users\sean\git\pirogue-framework\example-site\sql\data\{0}.csv" -f $name);
+}
