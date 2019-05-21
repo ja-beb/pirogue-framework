75d9b3b5a2067755cba25fe660780810c163a7b0
Sean Bourg <sean.bourg@gmail.com>
2019-05-21 16:24:31 -0500

    added test functions for site api

---
+++ test/example-site/json/site/users.ps1
@@ -0,0 +1,49 @@
+﻿<#
+    Author_____: Sean Bourg <sean.bourg@gmail.com>
+    Date_______: 2019-05-20
+    Description: Test org\users
+ #>
+ 
+using module ..\Modules\TestResults.psm1;
+using module ..\Modules\Test-Assert.psm1;
+
+$test = @();
+
+## Get department list
+$list = [TestResults]::Execute('site/users');
+$test += Test-Assert -Label $list.Url -TestResults ( [string]::IsNullOrEmpty($list.ErrorMessage) ) -Fail "Request returned error = $($list.ErrorMessage)" -Pass 'Request returned no error.' ;
+$test += Test-Assert -Label $list.Url -TestResults ( $null -ne $list.Data -and $list.Data.Count -gt 0 ) -Fail 'Request returned no data.' -Pass ( 'Request returned results ({0}).' -f $list.Data.Count);
+
+## Get test case
+$testCase = $list.Data[0];
+
+$result = [TestResults]::Execute( "site/users/$($testCase.id)" );
+$test += Test-Assert -Label $result.Url -TestResults ( [string]::IsNullOrEmpty($result.ErrorMessage) ) -Fail "Request returned error = $($result.ErrorMessage)" -Pass 'Request returned no error.' ;
+$test += Test-Assert -Label $result.Url -TestResults ( $null -ne $result.Data -and $result.Data.Count -gt 0 ) -Fail 'Request returned no data.' -Pass "Request returned result=$($result.Data).";
+
+## fetch - invalid
+$result = [TestResults]::Execute( 'site/users/1000000' );
+$test += Test-Assert -Label $result.Url -TestResults ( [string]::IsNullOrEmpty($result.ErrorMessage) -eq $false ) -Pass "Request returned error = $($result.ErrorMessage)" -Fail 'Request returned no error.' ;
+$test += Test-Assert -Label $result.Url -TestResults ( $null -eq $result.Data -or $result.Data.Count -eq 0 ) -Pass 'Request returned no data.' -Fail "Request returned result=$($result.Data).";
+
+## Get id lookup 
+$result = [TestResults]::Execute( "site/users/id/$($testCase.id)" );
+$test += Test-Assert -Label $result.Url -TestResults ( [string]::IsNullOrEmpty($result.ErrorMessage) ) -Fail $("Request returned error = $($result.ErrorMessage)") -Pass 'Request returned no error.' ;
+$test += Test-Assert -Label $result.Url -TestResults ( $null -ne $result.Data -and $result.Data.Count -gt 0 ) -Fail 'Request returned no data.' -Pass "Request returned result=$($result.Data).";
+
+## Get id lookup - invalid
+$result = [TestResults]::Execute( 'site/users/id/1000000' );
+$test += Test-Assert -Label $result.Url -TestResults ( [string]::IsNullOrEmpty($result.ErrorMessage) -eq $false ) -Pass "Request returned error = $($result.ErrorMessage)" -Fail 'Request returned no error.' ;
+$test += Test-Assert -Label $result.Url -TestResults ( $null -eq $result.Data -or $result.Data.Count -eq 0 ) -Pass 'Request returned no data.' -Fail "Request returned result=$($result.Data).";
+
+## Get email lookup
+$result = [TestResults]::Execute( "site/users/email/$($testCase.email)" );
+$test += Test-Assert -Label $result.Url -TestResults ( [string]::IsNullOrEmpty($result.ErrorMessage) ) -Fail $("Request returned error = $($result.ErrorMessage)") -Pass 'Request returned no error.' ;
+$test += Test-Assert -Label $result.Url -TestResults ( $null -ne $result.Data -and $result.Data.Count -gt 0) -Fail 'Request returned no data.' -Pass "Request returned result=$($result.Data).";
+
+## Get email lookup with invalid data
+$result = [TestResults]::Execute( 'org/users/email/invalid_account@hotmail.com' );
+$test += Test-Assert -Label $result.Url -TestResults ( [string]::IsNullOrEmpty($result.ErrorMessage) -eq $false ) -Pass "Request returned error = $($result.ErrorMessage)" -Fail 'Request returned no error.' ;
+$test += Test-Assert -Label $result.Url -TestResults ( $null -eq $result.Data -or $result.Data.Count -eq 0) -Pass 'Request returned no data.' -Fail "Request returned result=$($result.Data).";
+
+$test | Select Status, Label, Message | Out-GridView
