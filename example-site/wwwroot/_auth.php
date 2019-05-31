9966362d26fa89988634eacd84120e2f59115130
Sean Bourg <sean.bourg@gmail.com>
2019-05-31 17:42:51 -0500

    Updated login pages

--- example-site/wwwroot/_auth.php
+++ example-site/wwwroot/_auth.php
@@ -4,9 +4,12 @@ $GLOBALS['._auth.dispatcher.start_time'] = microtime(true);
 use function pirogue\__database_collection;
 use function pirogue\__dispatcher;
 use function pirogue\__import;
+use function pirogue\__route;
+use function pirogue\_dispatcher_exit;
 use function pirogue\_dispatcher_send;
-use function pirogue\import;
 use function pirogue\_route_clean;
+use function pirogue\_route_parse;
+use function pirogue\import;
 
 /**
  * Main dispatcher for AUTH content.
@@ -34,18 +37,12 @@ function _route_execute(string $file, string $path, array $data): string
     throw new ErrorException(sprintf("Unable to find requested resource '$file'."));
 }
 
-function _route_parse(string $base, string $path): array
-{
-    $_route = pirogue\_route_parse($base, $path);
-    $_route['file'] = "{$_route['file']}.phtml";
-    return $_route;
-}
-
 define('_BASE_FOLDER', 'C:\\inetpub\example-site');
 
 // Load & intialize pirogue framework:
 require_once sprintf('%s\include\pirogue\import.inc', _BASE_FOLDER);
 __import(sprintf('%s\include', _BASE_FOLDER));
+ob_start();
 
 try {
 
@@ -57,25 +54,28 @@ try {
     set_error_handler('pirogue\_dispatcher_error_handler');
 
     $GLOBALS['._pirogue.dispatcher.failsafe_exception'] = null;
-    $GLOBALS['._pirogue.dispatcher.controller_path'] = sprintf('%s\view\html-auth', _BASE_FOLDER);
 
-    /* Initialize libraries: */
-    __dispatcher(sprintf('%s://%s/example-site/auth', ('off' == $_SERVER['HTTPS']) ? 'http' : 'https', $_SERVER['SERVER_NAME']));
-    __database_collection(sprintf('%s\config', _BASE_FOLDER));
+    session_start(parse_ini_file(sprintf('%s\config\session.ini', _BASE_FOLDER)));
+    register_shutdown_function('session_write_close');
 
     /* Parse request */
     $_request_data = $_GET;
     $_request_path = $_request_data['__execution_path'] ?? '';
     unset($_request_data['__execution_path']);
 
+    /* Initialize libraries: */
+    __route(sprintf('%s\view\html-auth', _BASE_FOLDER), 'phtml');
+    __dispatcher(sprintf('%s://%s/example-site/auth', ('off' == $_SERVER['HTTPS']) ? 'http' : 'https', $_SERVER['SERVER_NAME']), $_request_path, $_request_data);
+    __database_collection(sprintf('%s\config', _BASE_FOLDER));
+
     // Route path to controller file, function & path:
     $_exec_data = $_request_data;
     $_exec_path = _route_clean($_request_path);
-    $_route = _route_parse($GLOBALS['._pirogue.dispatcher.controller_path'], $_exec_path);
+    $_route = _route_parse($_exec_path);
     $_content = '';
-    
+
     if (false == file_exists($_route['file'])) {
-        $_route = _route_parse($GLOBALS['._pirogue.dispatcher.controller_path'], '_error-404');
+        $_route = _route_parse('_error-404');
         $_exec_data = [
             $_request_path,
             $_request_data
@@ -88,8 +88,9 @@ try {
         $_content = _route_execute($_route['file'], $_route['path'], $_exec_data);
         ob_clean();
     } catch (Exception $_exception) {
+        ob_clean();
         http_response_code(500);
-        $_route = _route_parse($GLOBALS['._pirogue.dispatcher.controller_path'], '_error-500');
+        $_route = _route_parse('_error-500');
         $_exec_data = [
             $_request_path,
             $_request_data,
@@ -100,8 +101,9 @@ try {
         $_content = _route_execute($_route['file'], $_route['path'], $_exec_data);
         ob_clean();
     } catch (Error $_exception) {
+        ob_clean();
         http_response_code(500);
-        $_route = _route_parse($GLOBALS['._pirogue.dispatcher.controller_path'], '_error-500');
+        $_route = _route_parse('_error-500');
         $_exec_data = [
             $_request_path,
             $_request_data,
@@ -134,4 +136,4 @@ if ($GLOBALS['._pirogue.dispatcher.failsafe_exception']) {
 } else {
     echo 'Unknown exception encountered';
 }
-
+_dispatcher_exit();
