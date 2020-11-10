af2c2cf31db3c3fdfaf8f39d5edc7906f3c8622b
Sean Bourg <sean.bourg@gmail.com>
2020-11-10 16:59:34 -0600

    Updated docker structure and included pirogue framework

---
+++ test/app/include/testing/dispatcher.php
@@ -0,0 +1,15 @@
+<?php
+namespace test;
+
+// Test dispatcher_set_cache_control(int $code, int $max_age = 0)
+
+
+// Test dispatcher_init(string $address)
+
+// Test _dispatcher_cache(): string
+
+
+// Test dispatcher_send(string $content)
+
+// Test dispatcher_redirect(string $address, int $code = 301)
+
