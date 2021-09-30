adf78d6143f0f28201b7b6b88e1f1906aa605298
Sean Bourg <sean.bourg@gmail.com>
2021-09-30 15:52:09 -0500

    added testing & validation directory structure

---
+++ pirogue_testing/cdn.php
@@ -0,0 +1,37 @@
+<?php 
+    /**
+     * php version 8.0.0
+     * 
+     * @author Bourg, Sean <sean.bourg@gmail.com>
+     * @category
+     * @package
+     * @license
+     * @version
+     * @link
+     */
+
+    namespace pirogue_testing\cdn;
+    use pirogue;
+
+    function cdn_test(array $list, int $count) : void
+    {
+        __cdn($list);
+        var_dump($GLOBALS['._pirogue.cdn.address_list']);        
+        
+        for ( $i = 0; $i < $count; $i++ ){            
+            var_dump($GLOBALS['._pirogue.cdn.current_index']);
+            printf( '%d) %s', $i+1, cdn_create_url('test', ['key' => 'one', 'value' => 1]));
+            var_dump($GLOBALS['._pirogue.cdn.current_index']);
+        }
+    }
+    
+    cdn_test([],3);
+    cdn_test(['cdn.localhost'],4);
+    cdn_test([
+        'cdn1.localhost',
+        'cdn2.localhost',
+        'cdn3.localhost',
+        'cdn4.localhost',        
+    ],12);
+    
+    pirogue\cdn
