8342eedafceac0171bcd2d561978bfae61fb6105
Sean Bourg <sean.bourg@gmail.com>
2021-10-02 11:33:04 -0500

    updated docker container to only install and run code sniffer if flag CODE_SNIFFER=true

--- container/entrypoint.sh
+++ container/entrypoint.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 # Download codesniffer and execute run command.
 
-echo "[entry] download code sniffer."
-cd /project
-curl -OL https://squizlabs.github.io/PHP_CodeSniffer/phpcs.phar
-curl -OL https://squizlabs.github.io/PHP_CodeSniffer/phpcbf.phar
+
+if [ true = "${CODE_SNIFFER}" ]
+then
+    echo "[entry] download code sniffer."
+    cd /project
+    curl -OL https://squizlabs.github.io/PHP_CodeSniffer/phpcs.phar
+    curl -OL https://squizlabs.github.io/PHP_CodeSniffer/phpcbf.phar
+fi
+
 echo "[entry] exec run command $@"
 exec "$@"
--- container/run.sh
+++ container/run.sh
@@ -2,10 +2,15 @@
 # Run code sniffer check and repair.
 # Run all tests.
 
-echo "[run]::codesniffer start"
-php /project/phpcbf.phar --standard=/project/phpcs.xml /project/pirogue
-php /project/phpcs.phar --standard=/project/phpcs.xml /project/pirogue
-echo "[run]::codesniffer completed"
+if [ true = "${CODE_SNIFFER}" ]
+then
+    echo "[run]::codesniffer start"
+    php /project/phpcbf.phar --standard=/project/phpcs.xml /project/pirogue
+    php /project/phpcs.phar --standard=/project/phpcs.xml /project/pirogue
+    echo "[run]::codesniffer completed"
+fi    
+
+
 
 echo "[run]::_all.php start"
 php /project/testing/_all.php
