f08ab478b762f84e3a9631d818863ded9691c40a
Sean Bourg <sean.bourg@gmail.com>
2021-10-02 16:55:11 -0500

    Updated CDN tests

--- pirogue_testing/unit-test/cdn.php
+++ pirogue_testing/unit-test/cdn.php
@@ -1,34 +1,62 @@
 <?php 
     /**
+     * Test CDN server.
      * php version 8.0.0
      * 
      * @author Bourg, Sean <sean.bourg@gmail.com>
-     * @category
-     * @package
-     * @license
-     * @version
-     * @link
      */
 
-    function cdn_test(array $list, int $count) : void
+    require_once(join(DIRECTORY_SEPARATOR, [_PIROGUE_TESTING_LIB_PATH, 'cdn.inc']));
+
+    /**
+     * Define testing function
+     * @param array $server_list List of CDN servers.
+     * @param int $count Number of times to preform test.
+     * @return array list of testing errors.
+     */
+    function cdn_test(array $server_list, int $count): array
     {
-        __cdn($list);
-        var_dump($GLOBALS['._pirogue.cdn.address_list']);        
+        pirogue_cdn_init($server_list);
+        $_cdn_count = count($server_list);
+        
+        if (!empty(array_diff($server_list, $GLOBALS['._pirogue.cdn.address_list']))) {
+            return ['pirogue_cdn_init(): failed to initialize CDN server list.'];
+        }
         
         for ( $i = 0; $i < $count; $i++ ){            
-            var_dump($GLOBALS['._pirogue.cdn.current_index']);
-            printf( '%d) %s', $i+1, cdn_create_url('test', ['key' => 'one', 'value' => 1]));
-            var_dump($GLOBALS['._pirogue.cdn.current_index']);
+            $_error_list = [];
+            $_start_index = $GLOBALS['._pirogue.cdn.current_index'];
+            $_uri = pirogue_cdn_create_url('test', ['key' => 'one', 'value' => 1]);
+
+            if ($_start_index >= $_cdn_count && $_start_index !== 0) {
+                array_push($_error_list, 'pirogue_cdn_create_url(): Start index state exceeds list length.');
+            }
+
+            if ($GLOBALS['._pirogue.cdn.current_index'] >= $_cdn_count) {
+                array_push($_error_list, 'pirogue_cdn_create_url(): End index state exceeds list length.');
+            }
+
+            if (!str_starts_with($_uri, $GLOBALS['._pirogue.cdn.address_list'][$_start_index])) {
+                array_push($_error_list, 'pirogue_cdn_create_url(): URI returned is not the expected address.');
+            }
+            
+            if ( ($GLOBALS['._pirogue.cdn.current_index']-$_start_index) != 1 && 0 != $GLOBALS['._pirogue.cdn.current_index']) {
+                array_push($_error_list, sprintf('pirogue_cdn_create_url(): Index not properly incremented. %d, %d', $_start_index, $GLOBALS['._pirogue.cdn.current_index']));
+            }
+
+            if (!empty($_error_list)) {
+                return $_error_list;
+            }
         }
+        return [];
     }
     
-    cdn_test([],3);
-    cdn_test(['cdn.localhost'],4);
-    cdn_test([
-        'cdn1.localhost',
-        'cdn2.localhost',
-        'cdn3.localhost',
-        'cdn4.localhost',        
-    ],12);
-    
-    pirogue\cdn
+    // Run test
+    pirogue_test_run('CDN-00x03', fn() => cdn_test([],3));
+    pirogue_test_run('CDN-01x04', fn() => cdn_test(['cdn.localhost'],4));
+    pirogue_test_run('CDN-04x12',fn() => cdn_test([
+        'cdn.00.localhost',
+        'cdn.01.localhost',
+        'cdn.02.localhost',
+        'cdn.03.localhost',        
+    ],12));    
