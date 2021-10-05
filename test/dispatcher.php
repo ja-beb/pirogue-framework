5dd18c12ae210246c481ee7dde6982f5b0228c54
Sean Bourg <sean.bourg@gmail.com>
2021-10-04 19:49:16 -0500

    database collection testing.

--- test/dispatcher.php
+++ test/dispatcher.php
@@ -8,7 +8,30 @@
 
     require_once(join(DIRECTORY_SEPARATOR, [_PIROGUE_TESTING_LIB_PATH, 'dispatcher.php']));
 
-    // test pirogue_dispatcher_init
-    # pirogue_test_run('dispatcher-00', fn() => cdn_test([],3));
+    
+    pirogue_test_execute('pirogue_dispatcher_init - ', function() {
+        $address = 'https://test-site.localhost';
+        $request_path = 'users/list';
+        $request_data = ['id' => '1'];
 
-    // test pirogue_dispatcher_create_url
+        pirogue_dispatcher_init($address, $request_path, $request_data);
+        
+
+        $errors = [];
+        if ( $address !== $GLOBALS['.pirogue.dispatcher.address'] ) {
+            array_push($errors, 'Unable to set address.');
+        }
+
+        if ( $request_path !== $GLOBALS['.pirogue.dispatcher.request_path'] ) {
+            array_push($errors, 'Unable to set request path.');
+        }
+        
+        if ( ($request_data['id'] ?? null) != ($GLOBALS['.pirogue.dispatcher.request_data']['id'] ?? null) ){
+            array_push($errors, 'Unable to set request data.');
+        }
+
+        return $errors;
+    };
+
+
+    // pirogue_dispatcher_create_url
