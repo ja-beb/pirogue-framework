b75682ad63e9a602887fbee677b73464f0c98a01
Sean Bourg <sean.bourg@gmail.com>
2021-10-05 07:49:39 -0500

    Updated submodules.

--- container/entrypoint.sh
+++ container/entrypoint.sh
@@ -8,8 +8,15 @@ if [ true = "${CODE_SNIFFER}" ]
 then
     echo "[entry] download code sniffer."
     cd /project
-    curl -OL https://squizlabs.github.io/PHP_CodeSniffer/phpcs.phar
-    curl -OL https://squizlabs.github.io/PHP_CodeSniffer/phpcbf.phar
+    if [[ -f /project/phpcs.phar ]]
+    then
+        curl -OL https://squizlabs.github.io/PHP_CodeSniffer/phpcs.phar
+    fi
+
+    if [[ -f /project/phpcbf.phar ]]
+    then
+        curl -OL https://squizlabs.github.io/PHP_CodeSniffer/phpcbf.phar
+    fi
 fi
 
 echo "[entry] exec run command $@"
