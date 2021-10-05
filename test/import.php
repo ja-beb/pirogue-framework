cab650e3e0ea8d5f448f5e82cbd06afb4dce6ce0
Sean Bourg <sean.bourg@gmail.com>
2021-10-04 20:15:18 -0500

    created import testing.

--- test/import.php
+++ test/import.php
@@ -6,9 +6,24 @@
      * @author Bourg, Sean <sean.bourg@gmail.com>
      */
 
-    require_once(join(DIRECTORY_SEPARATOR, [_PIROGUE_TESTING_LIB_PATH, 'dispatcher.php']));
+    require_once(join(DIRECTORY_SEPARATOR, [_PIROGUE_TESTING_LIB_PATH, 'import.php']));
 
-    // test pirogue_import_init
-    # pirogue_test_run('dispatcher-00', fn() => cdn_test([],3));
-
-    // test pirogue_import
+    pirogue_test_execute('pirogue_import_init - invalid directory', function () {
+        try {
+            pirogue_import_init('no-where'));
+            return ['Set library to invalid file.'];
+        } catch (InvalidArgumentException $_exception) {
+            return [];
+        }
+    });
+    pirogue_test_execute('pirogue_import_init - valid directory', fn() => pirogue_import_init(_PIROGUE_TESTING_LIB_PATH));
+    pirogue_test_execute('pirogue_import_load - invalid file', function () {
+        try {
+            pirogue_import_load('file-not-found')
+            return [ 'Loaded invalid library file.']
+        } catch (ErrorException $_exception) {
+            return [];
+        }
+        
+    });
+    pirogue_test_execute('pirogue_import_load - valid file', fn() => pirogue_import_load('cdn'));
