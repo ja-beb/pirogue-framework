9a930668696b6174dacf709daed9bcfdc38e0f50
Sean Bourg <sean.bourg@gmail.com>
2021-10-07 08:55:42 -0500

    streamlined dispatcher logic. added pirogue library init.

--- test/site/html/_dispatcher.php
+++ test/site/html/_dispatcher.php
@@ -1,6 +1,7 @@
 <?php 
-
+    // bootstrap page - start necessary php functionality.
     ob_start();
+    session_id() || session_start();
     
     // define constants.
     define('_PIROGUE_PATH', '/var/www');
@@ -8,116 +9,90 @@
     define('_PIROGUE_PATH_VIEW', implode(DIRECTORY_SEPARATOR, [_PIROGUE_PATH, 'view']));
     define('_PIROGUE_PATH_CONFIG', implode(DIRECTORY_SEPARATOR, [_PIROGUE_PATH, 'config']));
 
+    function _dispatcher_clear_buffer() {
+        while ( 0 < ob_get_level() ) {
+            ob_get_clean();
+        }
+    }
+
     // load autoloader.
     require implode(DIRECTORY_SEPARATOR, [_PIROGUE_PATH_INCLUDE, 'pirogue', 'import.php']);
 
     try {
         pirogue_import_init(_PIROGUE_PATH_INCLUDE);
 
-        // load and register error handler.
+        // import libraries.            
         pirogue_import_load('pirogue/error_handler');
+        pirogue_import_load('pirogue/dispatcher');
+        pirogue_import_load('pirogue/view');
+        pirogue_import_load('pirogue/cdn');
+        pirogue_import_load('pirogue/site_notices');
+        pirogue_import_load('pirogue/database_collection');
+        pirogue_import_load('site/html');
+
+        // register error handler.
         set_error_handler('_pirogue_error_handler');
 
-        
         try {
-            $_request_data = $_GET;
-            $_request_path = $_request_data['__execution_path'] ?? '';
-            unset($_request_data['__execution_path']);
 
-            // init dispatcher.
-            pirogue_import_load('pirogue/dispatcher');
-            pirogue_dispatcher_init(sprintf('%s://%s', $_SERVER['REQUEST_SCHEME'], $_SERVER['HTTP_HOST']), $_request_path, $_request_data);
+            // parse request data.
+            $GLOBALS['.request_data'] = $_GET;
+            unset($GLOBALS['.request_data']['__execution_path']);
+            $GLOBALS['._dispatcher_path'] = explode('/', $_GET['__execution_path'] ?? '');
+            array_walk(
+                $GLOBALS['._dispatcher_path'],
+                fn($str, $key) => $GLOBALS['._dispatcher_path'][$key] = preg_replace('/^_/', '', $str)
+            );
 
-            // clean reqeust data.
-            $_data = $_request_data;
-            $_path_parts = explode('/', $_request_path);
-            array_walk($_path_parts, fn($str) => preg_replace('/^_/','', $str));
-            
-            $_module = array_shift($_path_parts);            
-            $_page = array_shift($_path_parts);            
-            $_page = empty($_page) ? 'index' : $_page;
+            // init dispatcher, cdn and view libraries.
+            pirogue_dispatcher_init(
+                sprintf('%s://%s', $_SERVER['REQUEST_SCHEME'], $_SERVER['HTTP_HOST']), 
+                implode('/', $GLOBALS['._dispatcher_path']), 
+                $GLOBALS['.request_data']
+            );            
+            pirogue_cdn_init([
+                sprintf('%s://%s', $_SERVER['REQUEST_SCHEME'], $_SERVER['HTTP_HOST'])
+            ]);
+            pirogue_view_init(_PIROGUE_PATH_VIEW);
+            pirogue_site_notices_init('._pirogue-testing.site_notices');
+            pirogue_database_collection_init(_PIROGUE_PATH_CONFIG, 'website');
 
-            if ( empty($_module) ){
-                $_view = $_page;
-            } else {
-                $_view = implode('/', [$_module, $_page]);
-            }
+            // build request components.
+            $GLOBALS['.request_path'] = $GLOBALS['._dispatcher_path'];
+            $GLOBALS['.request_module'] = array_shift($GLOBALS['.request_path']);
+            $GLOBALS['.request_page'] = array_shift($GLOBALS['.request_path']) ?? 'index';            
 
-            $_path = implode('/', $_path_parts);
-
-            pirogue_import_load('pirogue/view');
-            pirogue_import_load('pirogue/cdn');
-            pirogue_import_load('site/html');
-            
-            pirogue_view_init(_PIROGUE_PATH_VIEW);    
-            pirogue_cdn_init([$GLOBALS['.pirogue.dispatcher.address']]);
-
-            // initialize view variables:
+            // initialize html view variables:
             site_html_create();
-            
-            // load requested page.
-            $_view_file = _pirogue_view_get_path($_view);
-            $GLOBALS['.request_page'] = $_view;
-            $GLOBALS['.request_path'] = $_path;
-            $GLOBALS['.request_data'] = $_request_data;
-            
-
-            if ( empty($_view_file) ) {
-                $GLOBALS['.request_page'] = '_error-404';
-                $GLOBALS['.request_path'] = '';
-                $GLOBALS['.request_data'] = [
-                    'view_file' => $_view_file,
-                    'view' => $_view,
-                    'path' => $_path,
-                    'data' => $_data,
-                ];            
-                $_view_file = _pirogue_view_get_path('_error-404');
-                
-            }
-
-            $GLOBALS['view_data'] = [];
             ob_start();
-            require $_view_file;
+            $_path = empty($GLOBALS['.request_module']) ? $GLOBALS['.request_page'] : implode('/', [$GLOBALS['.request_module'], $GLOBALS['.request_page']]);
+            require _pirogue_view_get_path($_path) ?? _pirogue_view_get_path('_error/404');
             $GLOBALS['.html.body.content'] = ob_get_clean();
 
-            
         }
         catch (Throwable $_throwable) {
-            $_view_file = _pirogue_view_get_path('_error-500');
-            $GLOBALS['.request_page'] = '_error-500';
-            $GLOBALS['.request_path'] = '';
             $GLOBALS['.request_data'] = [
-                'exception' => $_throwable,
-                'view' => $_view,
-                'path' => $_path,
-                'data' => $_data,
-            ];            
+                'exception' => $_throwable
+            ];
 
-            $GLOBALS['view_data'] = [];
+            // load error view.
+            site_html_create();
             ob_start();
-            require $_view_file;
+            require _pirogue_view_get_path('_error/500');
             $GLOBALS['.html.body.content'] = ob_get_clean();        
         }
 
-
         // load conent into page template
-        $_view = _pirogue_view_get_path('_page');
-        
         ob_start();
-        require $_view;
+        require _pirogue_view_get_path('_page');
         $_html_page = ob_get_clean();
 
-
         // send page content and exit.
-        while ( 0 < ob_get_level() ) {
-            ob_get_clean();
-        }
+        _dispatcher_clear_buffer();
         _pirogue_dispatcher_send($_html_page);
         _pirogue_dispatcher_exit();
     } catch (Throwable $_throwable) {
-        while ( 0 < ob_get_level() ) {
-            ob_get_clean();
-        }
+        _dispatcher_clear_buffer();
         printf('%s (%s:%d)', $_throwable->getMessage(),$_throwable->getFile(), $_throwable->getLine());
         echo '<pre>';
         var_dump($_throwable);
