55c9163d088647d5bb857f344a0904ee90596529
Sean Bourg <sean.bourg@gmail.com>
2021-10-07 07:39:35 -0500

    created menu template.

---
+++ test/site/view/info/_menu.php
@@ -0,0 +1,11 @@
+<?php 
+
+
+return [
+        'PHP Info' => [
+            'title' => 'View php info output.',
+            'label' => 'PHP Info',
+            'path' => 'php.html',
+            'request' => []
+        ]    
+    ];
