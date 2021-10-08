6ba5f8650e34dd69540af1ce87e0ea84b0fa9e71
Sean Bourg <sean.bourg@gmail.com>
2021-10-08 17:37:46 -0500

    added redirect test.

--- test/site/view/html/test/_menu.php
+++ test/site/view/html/test/_menu.php
@@ -1,3 +1,10 @@
 <?php
 
-    return [];
+    pirogue_import_load('site/menu');
+
+    return [
+        site_menu_create(
+            label: 'Test Redirect',
+            path: 'redirect.html'
+        ),
+    ];
