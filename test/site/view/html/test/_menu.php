2b06d84fd63067d5c62864d35d31ddb8c47a62b7
Sean Bourg <sean.bourg@gmail.com>
2021-10-08 17:45:31 -0500

    added 500 test page.

--- test/site/view/html/test/_menu.php
+++ test/site/view/html/test/_menu.php
@@ -3,8 +3,6 @@
     pirogue_import_load('site/menu');
 
     return [
-        site_menu_create(
-            label: 'Test Redirect',
-            path: 'redirect.html'
-        ),
+        site_menu_create( label: 'Test Redirect', path: 'redirect.html' ),
+        site_menu_create( label: 'Error 500', path: 'error-500.html' ),
     ];
