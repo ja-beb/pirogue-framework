6b5735b2819ca2ae31f9a68f48a066ba5050b1a6
Sean Bourg <sean.bourg@gmail.com>
2021-10-08 18:31:37 -0500

    added user session menu option and use of site menu function.

--- test/site/view/html/info/_menu.php
+++ test/site/view/html/info/_menu.php
@@ -1,32 +1,11 @@
 <?php 
 
+    pirogue_import_load('site/menu');
 
-return [
-    [
-        'title' => 'View php info output.',
-        'label' => 'PHP Info',
-        'path' => 'php.html',
-        'request' => []
-    ],
-    [
-        'title' => 'View session data.',
-        'label' => 'Session Data',
-        'path' => 'session.html',
-        'request' => []
-    ],
-    [
-        'title' => 'View framework data.',
-        'label' => 'Pirogue Framework Data',
-        'path' => 'pirogue-vars.html',
-        'request' => []
-    ],
-    [
-        'title' => 'View dispatcher data.',
-        'label' => 'Dispatcher Data',
-        'path' => 'dispatcher-vars/page/options.html',
-        'request' => [
-            'test1' => 'true',
-            'test2' => 'false'
-        ]
-    ]
-];
+    return [
+        site_menu_create( title:'View php info output.', label:'PHP Info', path:'php.html'),
+        site_menu_create( title:'View session data.', label:'Session Data', path:'session.html'),
+        site_menu_create( title:'View framework data.', label:'Pirogue Framework Data', path:'pirogue-vars.html'),
+        site_menu_create( title:'View dispatcher data.', label:'Dispatcher Data', path:'dispatcher-vars/page/options.html', request:[ 'test1' => 'true', 'test2' => 'false' ] ),
+        site_menu_create( title:'User Session', label:'User Sesion', path:'user-session.html'),
+    ];
