ce877cb70de09035c6303959bc0d1fcf63d99f5b
Sean Bourg <sean.bourg@gmail.com>
2021-10-08 18:07:54 -0500

    Added site notices testing.

--- test/site/view/html/test/_menu.php
+++ test/site/view/html/test/_menu.php
@@ -5,4 +5,5 @@
     return [
         site_menu_create( label: 'Test Redirect', path: 'redirect.html' ),
         site_menu_create( label: 'Error 500', path: 'error-500.html' ),
+        site_menu_create( label: 'Site Notices', path: 'site-notices.html' ),
     ];
