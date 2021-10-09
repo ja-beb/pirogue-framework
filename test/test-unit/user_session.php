9ce7e0478ccd9e52ad1002bfd325fe09c84ae506
Sean Bourg <sean.bourg@gmail.com>
2021-10-09 18:26:36 -0500

    added pirogue_user_session_remove() testing

--- test/test-unit/user_session.php
+++ test/test-unit/user_session.php
@@ -15,27 +15,27 @@ pirogue_test_execute('pirogue_user_session_init:', function () {
     $test_label = 'test-label';
     pirogue_user_session_init($test_label);
 
-    $_errors = [];
+    $errors = [];
     if (false == array_key_exists('._pirogue.user_session.label_user', $GLOBALS)) {
-        array_push($_errors, '00 - var "._pirogue.user_session.label_user" not initialized.');
+        array_push($errors, '00 - var "._pirogue.user_session.label_user" not initialized.');
     } elseif ("{$test_label}_user" != $GLOBALS['._pirogue.user_session.label_user']) {
-        array_push($_errors, '01 - var "._pirogue.user_session.label_user" not properly set.');
+        array_push($errors, '01 - var "._pirogue.user_session.label_user" not properly set.');
     }
 
     if (false == array_key_exists('._pirogue.user_session.label_data', $GLOBALS)) {
-        array_push($_errors, '02 - var "._pirogue.user_session.label_data" not initialized.');
+        array_push($errors, '02 - var "._pirogue.user_session.label_data" not initialized.');
     } elseif ("{$test_label}_data" != $GLOBALS['._pirogue.user_session.label_data']) {
-        array_push($_errors, '03 - var "._pirogue.user_session.label_data" not properly set.');
+        array_push($errors, '03 - var "._pirogue.user_session.label_data" not properly set.');
     }
 
     if (false == array_key_exists($GLOBALS['._pirogue.user_session.label_user'], $_SESSION)) {
         array_push(
-            $_errors,
+            $errors,
             "04 - session variable '{$GLOBALS['._pirogue.user_session.label_user']}' not initialized."
         );
     } elseif (null != $_SESSION[$GLOBALS['._pirogue.user_session.label_user']]) {
         array_push(
-            $_errors,
+            $errors,
             sprintf(
                 "05 - session variable '{$GLOBALS['._pirogue.user_session.label_user']}' not properly set (type=%s).",
                 gettype($_SESSION[$GLOBALS['._pirogue.user_session.label_user']])
@@ -45,12 +45,12 @@ pirogue_test_execute('pirogue_user_session_init:', function () {
 
     if (false == array_key_exists($GLOBALS['._pirogue.user_session.label_data'], $_SESSION)) {
         array_push(
-            $_errors,
+            $errors,
             "06 - session variable '{$GLOBALS['._pirogue.user_session.label_data']}' not initialized."
         );
     } elseif (!is_array($_SESSION[$GLOBALS['._pirogue.user_session.label_data']])) {
         array_push(
-            $_errors,
+            $errors,
             sprintf(
                 "07 - session variable '{$GLOBALS['._pirogue.user_session.label_data']}' not properly set (type=%s).",
                 gettype($_SESSION[$GLOBALS['._pirogue.user_session.label_data']])
@@ -58,52 +58,52 @@ pirogue_test_execute('pirogue_user_session_init:', function () {
         );
     } elseif (!empty($_SESSION[$GLOBALS['._pirogue.user_session.label_data']])) {
         array_push(
-            $_errors,
+            $errors,
             "08 - session variable '{$GLOBALS['._pirogue.user_session.label_data']}' not properly set."
         );
     }
-    return $_errors;
+    return $errors;
 });
 
 // test pirogue_user_session_current(), _pirogue_user_session_start() and _pirogue_user_session_end()
-pirogue_test_execute('pirogue_user_session_current:', function () {
-    $_errors = [];
+pirogue_test_execute('pirogue_user_session_current', function () {
+    $errors = [];
     $user = ['id' => 1];
 
     if (null != pirogue_user_session_current()) {
-        array_push($_errors, '00 - user session contains values before being set.');
+        array_push($errors, '00 - user session contains values before being set.');
     }
 
     _pirogue_user_session_start($user);
     if ($user != pirogue_user_session_current()) {
-        array_push($_errors, '01 - user session data not properly set.');
+        array_push($errors, '01 - user session data not properly set.');
     }
 
     _pirogue_user_session_end();
     if (null != pirogue_user_session_current()) {
-        array_push($_errors, '02 - user session data not properly destoryed.');
+        array_push($errors, '02 - user session data not properly destoryed.');
     }
 
-    return $_errors;
+    return $errors;
 });
 
 // Test pirogue_user_session_set()
-pirogue_test_execute('pirogue_user_session_set:', function () {
+pirogue_test_execute('pirogue_user_session_set', function () {
     $label = '!my label';
     $value = "@my value";
-    $_errors = [];
+    $errors = [];
 
     pirogue_user_session_set($label, $value);
     if (!array_key_exists($label, $_SESSION[$GLOBALS['._pirogue.user_session.label_data']])) {
-        array_push($_errors, "00 - '{$label}' not set.");
+        array_push($errors, "00 - '{$label}' not set.");
     } elseif ($value != $_SESSION[$GLOBALS['._pirogue.user_session.label_data']][$label]) {
-        array_push($_errors, "01 - '{$label}' contains the wrong value.");
+        array_push($errors, "01 - '{$label}' contains the wrong value.");
     }
-    return $_errors;
+    return $errors;
 });
 
 // function pirogue_user_session_get(string $label): ?string
-pirogue_test_execute('pirogue_user_session_get:', function () {
+pirogue_test_execute('pirogue_user_session_get', function () {
     $label = '!pirogue_user_session_get.key';
     $value = "@my value";
 
@@ -115,5 +115,23 @@ pirogue_test_execute('pirogue_user_session_get:', function () {
 });
 
 // function pirogue_user_session_remove(string $label): ?string
+pirogue_test_execute('pirogue_user_session_remove', function () {
+    $label = '!pirogue_user_session_remove.key';
+    $value = "@my value";
+    $errors = [];
+
+    $_SESSION[$GLOBALS['._pirogue.user_session.label_data']][$label] = $value;
+    $del_value = pirogue_user_session_remove($label);
+    if ($value != $del_value) {
+        array_push($errors, '00 - returned value does not match set value.');
+    }
+
+    if (array_key_exists($label, $_SESSION[$GLOBALS['._pirogue.user_session.label_data']])) {
+        array_push($errors, '00 - value not unset.');
+    }
+
+    return $errors;
+});
+
 // function pirogue_user_session_exists(string $label): bool
 // pirogue_user_session_clear(): array
