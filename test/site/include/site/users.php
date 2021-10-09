c145470e3ffcba73fd7ac37a4931c1adfa503726
Sean Bourg <sean.bourg@gmail.com>
2021-10-09 09:26:39 -0500

    updated users library.

--- test/site/include/site/users.php
+++ test/site/include/site/users.php
@@ -1,31 +1,33 @@
 <?php
 
-    function site_user_fetch_by_id(mysqli $database_connection, int $id ): ?array {
-        return site_user_fetch($database_connection, 'id', $id);
-    }
-
-    function site_user_fetch(mysqli $database_connection, string $key, string $value): ?array {
-        $sql_query = sprintf(
-            "SELECT id, username, email, password FROM users WHERE %s='%s' LIMIT 1",
-            $key,
-            mysqli_real_escape_string($database_connection, $value)
-        );
-        $sql_result = mysqli_query($database_connection, $sql_query);
-        if ( false != $sql_result ) {
-            $user_data = mysqli_fetch_assoc($sql_result);
-            mysqli_free_result($sql_result);
-        } else {
-            $user_data = null;
+    function _site_user_query(mysqli $database_connection, string $filter, string $order_by=''): array {
+        $sql_result = mysqli_query($database_connection, "SELECT id, username, email, password FROM users {$filter} {$order_by}");
+        if ( is_bool($sql_result) ){
+            return [];
         }
-        return $user_data;
-    }
-
-    function site_user_list(mysqli $database_connection): array {
-        $sql_result = mysqli_query($database_connection, 'SELECT id, username, email, password FROM users ORDER BY id ASC');
         $users = [];
-        while ( $user_data = mysqli_fetch_assoc($sql_result) ) {
-            array_push($users, $user_data);
+        while ( $data = mysqli_fetch_assoc($sql_result) ) {
+            array_push($users, $data);
         }
         mysqli_free_result($sql_result);
         return $users;
+    }
+
+    function site_user_fetch_by_id(mysqli $database_connection, int $id ): ?array {
+        $users = _site_user_query($database_connection, sprintf('WHERE id=%d', $id), 'LIMIT 1');
+        return empty($users) ? null : current($users);
+    }
+
+    function site_user_fetch(mysqli $database_connection, string $key, string $value): ?array {
+        $sql_filter = sprintf(
+            "WHERE %s='%s'",
+            $key,
+            mysqli_real_escape_string($database_connection, $value)
+        );
+        $users = _site_user_query($database_connection, $sql_filter, 'LIMIT 1');
+        return empty($users) ? null : current($users);
+    }
+
+    function site_user_list(mysqli $database_connection): array {
+        return _site_user_query($database_connection, '');
     }
