b120897fbb8d78317eb52a24edd821e60dfd4b2b
Sean Bourg <sean.bourg@gmail.com>
2021-10-09 18:38:03 -0500

    create site notices test

---
+++ test/test-unit/site_notices.php
@@ -0,0 +1,11 @@
+
+/**
+ * Test the site notices functions.
+ * php version 8.0.0
+ *
+ * @author Bourg, Sean <sean.bourg@gmail.com>
+ * @license https://opensource.org/licenses/GPL-3.0 GPL-v3
+ */
+
+require_once(implode(DIRECTORY_SEPARATOR, [_PIROGUE_TESTING_PATH_INCLUDE, 'pirogue', 'site_notices.php']));
+
