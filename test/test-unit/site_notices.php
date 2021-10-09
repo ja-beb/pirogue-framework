5b017f1116b2359efd23b7fe69d60fc2c864c113
Sean Bourg <sean.bourg@gmail.com>
2021-10-09 18:39:30 -0500

    create site notices test

--- test/test-unit/site_notices.php
+++ test/test-unit/site_notices.php
@@ -9,3 +9,17 @@
 
 require_once(implode(DIRECTORY_SEPARATOR, [_PIROGUE_TESTING_PATH_INCLUDE, 'pirogue', 'site_notices.php']));
 
+// test pirogue_site_notices_init(string $index): void
+pirogue_test_execute('pirogue_user_session_init()', function () {
+
+});
+
+// test pirogue_site_notices_clear(): array
+pirogue_test_execute('pirogue_site_notices_clear()', function () {
+
+});
+
+// test pirogue_site_notices_create
+pirogue_test_execute('pirogue_site_notices_create()', function () {
+
+});
