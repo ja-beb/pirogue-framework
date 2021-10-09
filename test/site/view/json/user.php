ed596f0eb55e61d09762a525e7debefe81cc95cc
Sean Bourg <sean.bourg@gmail.com>
2021-10-09 07:45:47 -0500

    updated json user endpoint to use site/users library and include uris to the user details.

--- test/site/view/json/user.php
+++ test/site/view/json/user.php
@@ -1,37 +1,35 @@
 <?php
+    
+    pirogue_import_load('site/users');
+        
+
     /*
      * Display user information. if user identifier is passed use specific user else all users.
      */
     function _get_user_info(mysqli $database_connection, string $key='', string $value=''): ?array {
-        $sql_filter = match($key) {
-            '' => '',
-            'id' => sprintf('WHERE %s=%d', $key, $value),
-            'email', 'username' => sprintf("WHERE %s='%s'", $key, mysqli_real_escape_string($database_connection, $value)),
-        };
-
-        $sql_query = "SELECT id, username, email FROM users {$sql_filter} ORDER BY id ASC";
-        $sql_result = mysqli_query($database_connection, $sql_query);
-        $users = [];
-        while ( $sql_data = mysqli_fetch_assoc($sql_result) ) {
-            array_push($users, $sql_data);
-        }
-        mysqli_free_result($sql_result);
-        return 1 == count($users) ? $users[0] : $users;
+        return [];
     }
 
     $_database_connection = pirogue_database_collection_get();
     $_page = array_shift($GLOBALS['.request_path']);
-
-    if (empty($GLOBALS['.request_path'])) {
-        return _get_user_info($_database_connection);
+    if ('' == $_page) {
+        $_users = [];
+        foreach ( site_user_list($_database_connection) as $_user ) {
+            $_user['urls'] = [
+                pirogue_dispatcher_create_url(sprintf('%s/%s.json', $GLOBALS['.request_page'], $_user['id']), []),
+                pirogue_dispatcher_create_url(sprintf('%s/username/%s.json', $GLOBALS['.request_page'], $_user['username']), []),
+                pirogue_dispatcher_create_url(sprintf('%s/id/%s.json', $GLOBALS['.request_page'], $_user['id']), [])
+            ];
+            array_push($_users, $_user);
+        }
+        return $_users;
     } elseif (intval($_page) == $_page){
-            return _get_user_info($_database_connection, 'id', intval($_page));
+        return site_user_fetch_by_id($_database_connection, intval($_page));
     } else {
         $_key = match(strtolower($_page)){
-            'email' => 'email',
             'id' => 'id',
             'username' => 'username',
             default => '',
         };
-        return _get_user_info($_database_connection, $_key, array_shift($GLOBALS['.request_path']));
+        return site_user_fetch($_database_connection, $_key, array_shift($GLOBALS['.request_path']));
     }
