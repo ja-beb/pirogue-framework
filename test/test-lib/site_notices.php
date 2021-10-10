24b39ad067563c3b376351f3e8fbbaf147a51437
Sean Bourg <sean.bourg@gmail.com>
2021-10-10 10:49:50 -0500

    Updated site notices testing: broke into individual files per function tested.

--- test/test-lib/site_notices.php
+++ test/test-lib/site_notices.php
@@ -1,7 +1,23 @@
 <?php
 
+/**
+ * Assist in testing user_session library.
+ * php version 8.0.0
+ *
+ * @author Bourg, Sean <sean.bourg@gmail.com>
+ * @license https://opensource.org/licenses/GPL-3.0 GPL-v3
+ */
+
+ /**
+  * Label to use for testing site notices.
+  * @var string  _PIROGUE_TESTING_SITE_NOTICES_LABEL
+  */
 define('_PIROGUE_TESTING_SITE_NOTICES_LABEL', '._pirogue-testing.site_notices.label');
 
+/**
+  * Collection of testing site notices.
+  * @var array $GLOBALS['.pirogue-testing.session_notices.notices']
+  */
 $GLOBALS['.pirogue-testing.session_notices.notices'] = [
     ['error', 'test error message.'],
     ['info', 'test info message.'],
