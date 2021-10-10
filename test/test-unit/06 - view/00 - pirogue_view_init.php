500973a79400f4edee50b5a903428420e75110d3
Sean Bourg <sean.bourg@gmail.com>
2021-10-10 16:57:01 -0500

    updated calls to pirogue_test_execute() call to return a single error message rather than an array of error messages.

--- test/test-unit/06 - view/00 - pirogue_view_init.php
+++ test/test-unit/06 - view/00 - pirogue_view_init.php
@@ -12,9 +12,9 @@ require_once(implode(DIRECTORY_SEPARATOR, [_PIROGUE_TESTING_PATH_INCLUDE, 'pirog
 pirogue_test_execute('pirogue_view_init(): invalid directory', function () {
     try {
         pirogue_view_init('/invalid/path');
-        return ['initialized to invalid directory.'];
+        return 'initialized to invalid directory.';
     } catch (InvalidArgumentException) {
-        return [];
+        return '';
     }
 });
 
@@ -26,13 +26,13 @@ pirogue_test_execute(
 pirogue_test_execute("pirogue_view_init(): \$GLOBALS['._pirogue.view.path']", function () {
     pirogue_view_init(_PIROGUE_TESTING_PATH_VIEW);
     return _PIROGUE_TESTING_PATH_VIEW == $GLOBALS['._pirogue.view.path']
-        ? []
-        : ['invalid view directory.'];
+        ? ''
+        : 'invalid view directory.';
 });
 
 pirogue_test_execute("pirogue_view_init(): \$GLOBALS['._pirogue.view.extension']", function () {
     pirogue_view_init(_PIROGUE_TESTING_PATH_VIEW, 'phtml');
     return 'phtml' == $GLOBALS['._pirogue.view.extension']
-        ? []
-        : ['invalid extension.'];
+        ? ''
+        : 'invalid extension.';
 });
