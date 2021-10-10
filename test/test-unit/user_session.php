008f3ea11b29e7c0e3499f9c3deac08ec8b2f3b5
Sean Bourg <sean.bourg@gmail.com>
2021-10-10 06:10:40 -0500

    added reset function and global index label.

--- test/test-unit/user_session.php
+++ test/test-unit/user_session.php
@@ -8,9 +8,13 @@
  * @license https://opensource.org/licenses/GPL-3.0 GPL-v3
  */
 
+ // load required library.
 require_once(implode(DIRECTORY_SEPARATOR, [_PIROGUE_TESTING_PATH_INCLUDE, 'pirogue', 'user_session.php']));
 
-// define test object
+// define testing constants.
+define('_PIROGUE_TESTING_USER_SESSION_LABEL', '._pirogue-testing.user_session.label');
+
+// define test object.
 class PirogueTestObject
 {
     public function __construct(
@@ -22,6 +26,12 @@ class PirogueTestObject
     public function __toString(): string {
         return '';
     }
+}
+
+function _user_session_reset(): void
+{
+    $_SESSION[_PIROGUE_TESTING_USER_SESSION_LABEL] = null;
+    $_SESSION[_PIROGUE_TESTING_USER_SESSION_LABEL] = [];
 }
 
 // initialize and reset session variable.
@@ -36,19 +46,18 @@ $GLOBALS['._pirogue-testing.user_session.list'] = [
 
 // test pirogue_user_session_init()
 pirogue_test_execute('pirogue_user_session_init:', function () {
-    $test_label = 'test-label';
-    pirogue_user_session_init($test_label);
+    pirogue_user_session_init(_PIROGUE_TESTING_USER_SESSION_LABEL);
 
     $errors = [];
     if (false == array_key_exists('._pirogue.user_session.label_user', $GLOBALS)) {
         array_push($errors, '00 - var "._pirogue.user_session.label_user" not initialized.');
-    } elseif ("{$test_label}_user" != $GLOBALS['._pirogue.user_session.label_user']) {
+    } elseif (sprintf('%s_user', _PIROGUE_TESTING_USER_SESSION_LABEL) != $GLOBALS['._pirogue.user_session.label_user']) {
         array_push($errors, '01 - var "._pirogue.user_session.label_user" not properly set.');
     }
 
     if (false == array_key_exists('._pirogue.user_session.label_data', $GLOBALS)) {
         array_push($errors, '02 - var "._pirogue.user_session.label_data" not initialized.');
-    } elseif ("{$test_label}_data" != $GLOBALS['._pirogue.user_session.label_data']) {
+    } elseif (sprintf('%s_data', _PIROGUE_TESTING_USER_SESSION_LABEL) != $GLOBALS['._pirogue.user_session.label_data']) {
         array_push($errors, '03 - var "._pirogue.user_session.label_data" not properly set.');
     }
 
@@ -91,6 +100,7 @@ pirogue_test_execute('pirogue_user_session_init:', function () {
 
 // test pirogue_user_session_current(), _pirogue_user_session_start() and _pirogue_user_session_end()
 pirogue_test_execute('pirogue_user_session_current', function () {
+    _user_session_reset();
     $errors = [];
     $user = ['id' => 1];
 
@@ -113,6 +123,7 @@ pirogue_test_execute('pirogue_user_session_current', function () {
 
 // Test pirogue_user_session_set()
 pirogue_test_execute('pirogue_user_session_set', function () {
+    _user_session_reset();
     $label = '!my label';
     $value = "@my value";
     $errors = [];
@@ -128,6 +139,7 @@ pirogue_test_execute('pirogue_user_session_set', function () {
 
 // function pirogue_user_session_get(string $label): ?string
 pirogue_test_execute('pirogue_user_session_get', function () {
+    _user_session_reset();
     $label = '!pirogue_user_session_get.key';
     $value = "@my value";
 
@@ -140,6 +152,7 @@ pirogue_test_execute('pirogue_user_session_get', function () {
 
 // function pirogue_user_session_remove(string $label): ?string
 pirogue_test_execute('pirogue_user_session_remove', function () {
+    _user_session_reset();
     $label = '!pirogue_user_session_remove.key';
     $value = "@my value";
     $errors = [];
@@ -159,6 +172,7 @@ pirogue_test_execute('pirogue_user_session_remove', function () {
 
 // function pirogue_user_session_exists(string $label): bool
 pirogue_test_execute('pirogue_user_session_exists', function () {
+    _user_session_reset();
     $label = '!pirogue_user_session_exists.key';
     $value = "@my value";
     $errors = [];
@@ -178,6 +192,7 @@ pirogue_test_execute('pirogue_user_session_exists', function () {
 
 // pirogue_user_session_clear(): array
 pirogue_test_execute('pirogue_user_session_clear', function () {
+    _user_session_reset();
     $saved_state = $_SESSION[$GLOBALS['._pirogue.user_session.label_data']];
     $errors = [];
     if ($saved_state != pirogue_user_session_clear()) {
