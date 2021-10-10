51fb4960d299a067241edf478788d305d698a47f
Sean Bourg <sean.bourg@gmail.com>
2021-10-10 09:05:35 -0500

    cleaned up pirogue_user_session_current() testing.

--- test/test-unit/01 - pirogue_user_session_current.php
+++ test/test-unit/01 - pirogue_user_session_current.php
@@ -13,51 +13,19 @@ require_once implode(DIRECTORY_SEPARATOR, [_PIROGUE_TESTING_PATH_INCLUDE, 'pirog
 require_once implode(DIRECTORY_SEPARATOR, [_PIROGUE_TESTING_PATH_INCLUDE, 'test', '_PirogueTestObject.php']);
 require_once implode(DIRECTORY_SEPARATOR, [_PIROGUE_TESTING_PATH_INCLUDE, 'test', 'user_session.php']);
 
-// define global variables.
-$GLOBALS['._pirogue-testing.user_session.user'] = [
-    'id' => 1,
-    '.username' => 'admin',
-    '@display name' => 'Admin User',
-];
-
-// test pirogue_user_session_current()
+// test pirogue_user_session_current() - check for emtpy initialized session
 pirogue_test_execute('pirogue_user_session_current', function () {
     _user_session_test_init(_PIROGUE_TESTING_USER_SESSION_LABEL, []);
-    $errors = [];
-
-        // check for user session before it is set.
-    if (null != pirogue_user_session_current()) {
-        array_push($errors, '00 - user session contains values before being set.');
-    }
-
-    // manually set user session.
-    $_SESSION[$GLOBALS['._pirogue.user_session.label_user']] = $GLOBALS['._pirogue-testing.user_session.user'];
-
-        // check for user session afte set.
-    if ($GLOBALS['._pirogue-testing.user_session.user'] != pirogue_user_session_current()) {
-        array_push($errors, '01 - user session data did not return proper value.');
-    }
-
-    return $errors;
+    return null == pirogue_user_session_current() ? [] : ['User session contains values before being set.'];
 });
 
-// test pirogue_user_session_current()
+// test pirogue_user_session_current() - check for session after set manually.
 pirogue_test_execute('pirogue_user_session_current', function () {
     _user_session_test_init(_PIROGUE_TESTING_USER_SESSION_LABEL, []);
-    $errors = [];
-
-    // check for user session before it is set.
-    if (null != pirogue_user_session_current()) {
-        array_push($errors, '00 - user session contains values before being set.');
-    }
-
-    // manually set user session.
     $_SESSION[$GLOBALS['._pirogue.user_session.label_user']] = $GLOBALS['._pirogue-testing.user_session.user'];
-
-    // check for user session afte set.
-    if ($GLOBALS['._pirogue-testing.user_session.user'] != pirogue_user_session_current()) {
-        array_push($errors, '01 - user session data did not return proper value.');
-    }
-
-    return $errors;
+    return match (pirogue_user_session_current()) {
+        null => ['User data not set.'],
+        !$GLOBALS['._pirogue-testing.user_session.user'] => ['Invalid user data returned.'],
+        default => []
+    };
 });
