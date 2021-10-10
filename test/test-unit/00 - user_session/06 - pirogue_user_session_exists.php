cd5316e2b05aa37f8f2bd7a61de8ab27935f7fcc
Sean Bourg <sean.bourg@gmail.com>
2021-10-10 15:15:04 -0500

    seperated out pirogue_user_session_exists() testing.

---
+++ test/test-unit/00 - user_session/06 - pirogue_user_session_exists.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * Test pirogue_user_session_exists().
+ * php version 8.0.0
+ *
+ * @author Bourg, Sean <sean.bourg@gmail.com>
+ * @license https://opensource.org/licenses/GPL-3.0 GPL-v3
+ */
+
+// load required library.
+require_once implode(DIRECTORY_SEPARATOR, [_PIROGUE_TESTING_PATH_INCLUDE, 'pirogue', 'user_session.php']);
+require_once implode(DIRECTORY_SEPARATOR, [_PIROGUE_TESTING_PATH_INCLUDE, 'test', 'user_session.php']);
+
+pirogue_test_execute('pirogue_user_session_exists(): verify values not in array are not returned.', function () {
+    $_SESSION = [];
+    pirogue_user_session_init(_PIROGUE_TESTING_USER_SESSION_LABEL);
+
+    // test for values before set.
+    foreach ($GLOBALS['._pirogue-testing.user_session.list'] as $key => $value) {
+        if (pirogue_user_session_exists($key)) {
+            return ["Value '{$key}' exists before set."];
+        }
+    }
+    return [];
+});
+
+pirogue_test_execute('pirogue_user_session_exists(): verify values are exist after being set. ', function () {
+    $_SESSION = [];
+    $errors = [];
+    pirogue_user_session_init(_PIROGUE_TESTING_USER_SESSION_LABEL);
+
+    // test for values before set.
+    foreach ($GLOBALS['._pirogue-testing.user_session.list'] as $key => $value) {
+        pirogue_user_session_set($key, $value);
+        if (!pirogue_user_session_exists($key)) {
+            return ["Value '{$key}' does not exists after being set."];
+        }
+    }
+    return [];
+});
