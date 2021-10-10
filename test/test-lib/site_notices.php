a8828c2744b3819556242ada24340eea9732af87
Sean Bourg <sean.bourg@gmail.com>
2021-10-10 10:31:20 -0500

    created site_notices lib file.

---
+++ test/test-lib/site_notices.php
@@ -0,0 +1,9 @@
+<?php
+
+define('_PIROGUE_TESTING_SITE_NOTICES_LABEL', '._pirogue-testing.site_notices.label');
+
+$GLOBALS['.pirogue-testing.session_notices.notices'] = [
+    ['error', 'test error message.'],
+    ['info', 'test info message.'],
+    ['warning', 'test warning message.'],
+];
