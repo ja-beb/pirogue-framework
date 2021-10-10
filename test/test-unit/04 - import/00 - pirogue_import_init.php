bbe4f0d8165ed98794b3a2535cfbbd9f35accce0
Sean Bourg <sean.bourg@gmail.com>
2021-10-10 16:02:21 -0500

    seperated function test to individual files.

---
+++ test/test-unit/04 - import/00 - pirogue_import_init.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Test pirogue_import_init().
+ * php version 8.0.0
+ *
+ * @author Bourg, Sean <sean.bourg@gmail.com>
+ */
+
+require_once(implode(DIRECTORY_SEPARATOR, [_PIROGUE_TESTING_PATH_INCLUDE, 'pirogue', 'import.php']));
+
+pirogue_test_execute('pirogue_import_init(): invalid directory', function () {
+    try {
+        pirogue_import_init('/invalid/path');
+        return ['Set library to invalid file.'];
+    } catch (InvalidArgumentException) {
+        return [];
+    }
+});
+
+pirogue_test_execute(
+    'pirogue_import_init(): valid directory',
+    fn() => pirogue_import_init(_PIROGUE_TESTING_PATH_INCLUDE)
+);
