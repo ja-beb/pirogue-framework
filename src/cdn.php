2201b999802417adbeac6381ead953b544c44fb5
Sean Bourg <sean.bourg@gmail.com>
2021-10-14 13:17:39 -0500

    refactor: changed index increment and cleaned up file.

--- src/cdn.php
+++ src/cdn.php
@@ -25,7 +25,7 @@ $GLOBALS['._pirogue.cdn.address_list'] = [];
 $GLOBALS['._pirogue.cdn.current_index'] = 0;
 
 /**
- * Setup CDN library.
+ * initialize the cdn library.
  *
  * @uses $GLOBALS['._pirogue.cdn.current_index']
  * @uses $GLOBALS['._pirogue.cdn.address_list']
@@ -41,7 +41,6 @@ function pirogue_cdn_init(array $address_list): void
  * Create url to resource relative to the cdn base.
  * Allocates CDN for usaged based on a round robin scheduling.
  *
- *
  * @throws LogicException if there are no registered CDN servers.
  * @uses $GLOBALS['._pirogue.cdn.address_list']
  * @uses $GLOBALS['._pirogue.cdn.current_index']
@@ -51,17 +50,13 @@ function pirogue_cdn_init(array $address_list): void
  */
 function pirogue_cdn_create_url(string $path, array $data): string
 {
-    // If no CDN servers are registered throw exception.
     if (empty($GLOBALS['._pirogue.cdn.address_list'])) {
         throw new LogicException('There are no registered CDN servers.');
     }
 
     $base_url = $GLOBALS['._pirogue.cdn.address_list'][$GLOBALS['._pirogue.cdn.current_index']];
-    $GLOBALS['._pirogue.cdn.current_index'] ++;
-    if (count($GLOBALS['._pirogue.cdn.address_list']) <= $GLOBALS['._pirogue.cdn.current_index']) {
-        $GLOBALS['._pirogue.cdn.current_index'] = 0;
-    }
-
+    $GLOBALS['._pirogue.cdn.current_index'] = 
+        ($GLOBALS['._pirogue.cdn.current_index'] + 1) % count($GLOBALS['._pirogue.cdn.address_list']);
     $url_pattern = empty($path) ? '%s' : '%s/%s';
     $base_url = sprintf($url_pattern, $base_url, $path);
     return (0 == count($data)) ? $base_url : sprintf('%s?%s', $base_url, http_build_query($data));
