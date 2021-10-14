d602123728642212d59294bd5e15c9aaa4872e37
Sean Bourg <sean.bourg@gmail.com>
2021-10-14 16:06:58 -0500

    fixed init call

--- test/test-unit/05 - view/00 - pirogue_view_init.php
+++ test/test-unit/05 - view/00 - pirogue_view_init.php
@@ -10,7 +10,7 @@
 require_once(implode(DIRECTORY_SEPARATOR, [_PIROGUE_TESTING_PATH_INCLUDE, 'pirogue', 'view.php']));
 
 pirogue_test_execute("pirogue_view_init(): \$GLOBALS['._pirogue.view.path']", function () {
-    $format = pirogue_view_init(sprintf('%s/%s.php', _PIROGUE_TESTING_PATH, '%s'));
+    $format = implode(DIRECTORY_SEPARATOR, [_PIROGUE_TESTING_PATH, 'view', '%s.phtml']);
     pirogue_view_init($format);
     return $format == $GLOBALS['._pirogue.view.path']
         ? ''
