b6cce11e9dfa51fded559aaa002f3c6644993d65
Sean Bourg <sean.bourg@gmail.com>
2021-10-14 16:06:41 -0500

    fixed init call and added testing for files outside of view folder.

--- test/test-unit/05 - view/01 - pirogue_view_get_path.php
+++ test/test-unit/05 - view/01 - pirogue_view_get_path.php
@@ -9,12 +9,19 @@
 
 require_once(implode(DIRECTORY_SEPARATOR, [_PIROGUE_TESTING_PATH_INCLUDE, 'pirogue', 'view.php']));
 
+pirogue_view_init(implode(DIRECTORY_SEPARATOR, [_PIROGUE_TESTING_PATH, 'view', '%s.phtml']));
+
 pirogue_test_execute('_pirogue_view_get_path: invalid file', function () {
     $view = pirogue_view_get_path('invalid-file');
-    return '' == $view ? '' : 'Loaded invalid view.';
+    return '' == $view ? '' : sprintf('unable to load view: "%s"', $view);
 });
 
 pirogue_test_execute('_pirogue_view_get_path: valid file', function () {
     $view = pirogue_view_get_path('test');
-    return '' == $view ? 'Unable to load view.' : '';
+    return '' == $view ? sprintf('unable to load view: "%s"', $view) : '';
+});
+
+pirogue_test_execute('_pirogue_view_get_path: ../config/invalid.phtml', function () {
+    $view = pirogue_view_get_path('../config/invalid');
+    return '' == $view ? sprintf('able to load view: "%s"', $view) : '';
 });
