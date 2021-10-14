8ba485da42adfd121f7cec406ec53f1a5549be7a
Sean Bourg <sean.bourg@gmail.com>
2021-10-14 16:25:18 -0500

    changed path to config constant to variable.

--- test/test-unit/04 - database_collection/00 - pirogue_database_collection_init.php
+++ test/test-unit/04 - database_collection/00 - pirogue_database_collection_init.php
@@ -7,14 +7,14 @@
  * @author Bourg, Sean <sean.bourg@gmail.com>
  */
 
-define('_PIROGUE_TESTING_PATH_CONFIG', implode(DIRECTORY_SEPARATOR, [_PIROGUE_TESTING_PATH, 'config']));
-
 require_once(implode(DIRECTORY_SEPARATOR, [_PIROGUE_TESTING_PATH, 'include', 'pirogue', 'database_collection.php']));
+
+$GLOBALS['.pirogue-testing.database_collection.config_path'] = implode(DIRECTORY_SEPARATOR, [_PIROGUE_TESTING_PATH, 'config']);
 
 pirogue_test_execute('pirogue_database_collection_init(): invalid config directory', function () {
     try {
         pirogue_database_collection_init(
-            implode(DIRECTORY_SEPARATOR, [_PIROGUE_TESTING_PATH_CONFIG, 'config-invalid']),
+            implode(DIRECTORY_SEPARATOR, [$GLOBALS['.pirogue-testing.database_collection.config_path'], 'config-invalid']),
             'website'
         );
         return 'Invalid database config directory accepted.';
@@ -25,25 +25,25 @@ pirogue_test_execute('pirogue_database_collection_init(): invalid config directo
 
 pirogue_test_execute(
     'pirogue_database_collection_init(): valid config directory',
-    fn() => pirogue_database_collection_init(_PIROGUE_TESTING_PATH_CONFIG, 'website')
+    fn() => pirogue_database_collection_init($GLOBALS['.pirogue-testing.database_collection.config_path'], 'website')
 );
 
 pirogue_test_execute("pirogue_database_collection_init(): \$GLOBALS['._pirogue.database_collection.config_path']", function () {
-    pirogue_database_collection_init(_PIROGUE_TESTING_PATH_CONFIG, 'website');
-    return _PIROGUE_TESTING_PATH_CONFIG == $GLOBALS['._pirogue.database_collection.config_path']
+    pirogue_database_collection_init($GLOBALS['.pirogue-testing.database_collection.config_path'], 'website');
+    return $GLOBALS['.pirogue-testing.database_collection.config_path'] == $GLOBALS['._pirogue.database_collection.config_path']
         ? ''
         : "invalid value for \$GLOBALS['._pirogue.database_collection.config_path']";
 });
 
 pirogue_test_execute("pirogue_database_collection_init(): \$GLOBALS['._pirogue.database_collection.default']", function () {
-    pirogue_database_collection_init(_PIROGUE_TESTING_PATH_CONFIG, 'website');
+    pirogue_database_collection_init($GLOBALS['.pirogue-testing.database_collection.config_path'], 'website');
     return 'website' == $GLOBALS['._pirogue.database_collection.default']
         ? ''
         : "invalid value for \$GLOBALS['._pirogue.database_collection.default']]";
 });
 
 pirogue_test_execute("pirogue_database_collection_init(): \$GLOBALS['._pirogue.database_collection.connections']", function () {
-    pirogue_database_collection_init(_PIROGUE_TESTING_PATH_CONFIG, 'website');
+    pirogue_database_collection_init($GLOBALS['.pirogue-testing.database_collection.config_path'], 'website');
     return empty($GLOBALS['._pirogue.database_collection.connections'])
         ? ''
         : "invalid value for \$GLOBALS['._pirogue.database_collection.connections']";
