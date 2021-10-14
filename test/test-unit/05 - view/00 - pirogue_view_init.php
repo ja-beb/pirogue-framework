67b9f599138f386ea512203946d0aa13cbd9009c
Sean Bourg <sean.bourg@gmail.com>
2021-10-14 16:10:44 -0500

    changed path to format check.

--- test/test-unit/05 - view/00 - pirogue_view_init.php
+++ test/test-unit/05 - view/00 - pirogue_view_init.php
@@ -12,7 +12,7 @@ require_once(implode(DIRECTORY_SEPARATOR, [_PIROGUE_TESTING_PATH_INCLUDE, 'pirog
 pirogue_test_execute("pirogue_view_init(): \$GLOBALS['._pirogue.view.path']", function () {
     $format = implode(DIRECTORY_SEPARATOR, [_PIROGUE_TESTING_PATH, 'view', '%s.phtml']);
     pirogue_view_init($format);
-    return $format == $GLOBALS['._pirogue.view.path']
+    return $format == $GLOBALS['._pirogue.view.format']
         ? ''
         : 'invalid view directory.';
 });
