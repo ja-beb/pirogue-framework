cc958bc441ac2707db54dd03ddc124263fc1c9d2
Sean Bourg <sean.bourg@gmail.com>
2021-10-28 11:19:18 -0500

    updated testing to reflect pattern

--- test/tests/04 - database_collection/02 - database_collection_get.php
+++ test/tests/04 - database_collection/02 - database_collection_get.php
@@ -10,9 +10,9 @@
 use function pirogue\database_collection_get;
 use function pirogue\database_collection_init;
 
-define('_PIROGUE_TESTING_PATH_CONFIG', implode(DIRECTORY_SEPARATOR, [_PIROGUE_TESTING_PATH, 'config']));
-
 require_once(implode(DIRECTORY_SEPARATOR, [_PIROGUE_TESTING_PATH, 'include', 'pirogue', 'database_collection.php']));
+
+database_collection_init(implode(DIRECTORY_SEPARATOR, [_PIROGUE_TESTING_PATH, 'config', 'mysqli-%s.ini']), 'website');
 
 function _pirogue_test_database_collection_query(?mysqli $database_connection)
 {
@@ -22,43 +22,27 @@ function _pirogue_test_database_collection_query(?mysqli $database_connection)
     return array_key_exists('user_count', $_sql_data) ? '' : 'unable to get count of website.users rows.';
 }
 
-pirogue_test_execute('pirogue_database_collection_init: invalid config directory', function () {
-    try {
-        database_collection_init(
-            implode(DIRECTORY_SEPARATOR, [_PIROGUE_TESTING_PATH_CONFIG, 'config-invalid']),
-            'website'
-        );
-        return 'Invalid database config directory accepted.';
-    } catch (InvalidArgumentException) {
-        return '';
-    }
-});
-
 pirogue_test_execute('database_collection_get(): invalid label', function () {
     try {
-        database_collection_init(_PIROGUE_TESTING_PATH_CONFIG, 'website');
         _pirogue_test_database_collection_query(database_collection_get('no-such-connection'));
         return 'Invalid database connection returned.';
-    } catch (ErrorException) {
+    } catch (Throwable) {
         return '';
     }
 });
 
 pirogue_test_execute('database_collection_get(): valid label', function () {
-    database_collection_init(_PIROGUE_TESTING_PATH_CONFIG, 'website');
     _pirogue_test_database_collection_query(database_collection_get('website'));
 });
 
 // test database_collection_get: default label
 pirogue_test_execute('database_collection_get(): default label', function () {
-    database_collection_init(_PIROGUE_TESTING_PATH_CONFIG, 'website');
     _pirogue_test_database_collection_query(database_collection_get());
 });
 
 // test database_collection_get: default label
 pirogue_test_execute('pirogue_database_collection_get(): website-invalid', function () {
     try {
-        database_collection_init(_PIROGUE_TESTING_PATH_CONFIG, 'website');
         _pirogue_test_database_collection_query(database_collection_get('website-invalid'));
         return 'invalid database connection returned.';
     } catch (Throwable) {
