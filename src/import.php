972a0c94c43f57a80de600504646cbf12794c803
Sean Bourg <sean.bourg@gmail.com>
2021-10-27 21:04:41 -0500

    added namespace for pirogue and removed function prefix

--- src/import.php
+++ src/import.php
@@ -9,6 +9,9 @@
  * @license https://opensource.org/licenses/GPL-3.0 GPL-v3
  */
 
+namespace pirogue;
+
+
 /**
  * Base folder for library import.
  *
@@ -23,7 +26,7 @@ $GLOBALS['._pirogue.import.path'] = '';
  * @uses $GLOBALS['._pirogue.import.path']
  * @param string $path the base path that contains *.php library files files.
  */
-function pirogue_import_init(string $path): void
+function import_init(string $path): void
 {
     if (!is_dir($path)) {
         throw new InvalidArgumentException(sprintf('Directory does not exist: "%s"', $path));
@@ -38,7 +41,7 @@ function pirogue_import_init(string $path): void
  * @throws ErrorException the error thrown if the requested library file is not found.
  * @param string $name the name of library being loaded (translates to the filename without extension).
  */
-function pirogue_import_load(string $name): void
+function import_load(string $name): void
 {
     $include_file = sprintf('%s.php', implode(DIRECTORY_SEPARATOR, [ $GLOBALS['._pirogue.import.path'], $name]));
     if (false == file_exists($include_file)) {
