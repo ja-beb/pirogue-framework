bbf8d7bae579b6aa8e113f168045efc7b6541046
Sean Bourg <sean.bourg@gmail.com>
2021-10-29 21:41:39 -0500

    expanded namespace.

--- src/import.php
+++ src/import.php
@@ -9,7 +9,7 @@
  * @license https://opensource.org/licenses/GPL-3.0 GPL-v3
  */
 
-namespace pirogue;
+namespace pirogue\import;
 
 use ErrorException;
 
@@ -17,32 +17,45 @@ use ErrorException;
  * the sprintf pattern used to build include file paths.
  *
  * @internal used by library only.
- * @var string $GLOBALS['._pirogue.import.pattern']
+ * @var string $GLOBALS['._pirogue.import.path_pattern']
  */
-$GLOBALS['._pirogue.import.pattern'] = '';
+$GLOBALS['._pirogue.import.path_pattern'] = '';
 
 /**
  * initialize the import library.
  *
- * @uses $GLOBALS['._pirogue.import.pattern']
- * @param string $pattern the file path pattern used to build include file paths.
+ * @uses $GLOBALS['._pirogue.import.path_pattern']
+ * @param string $path_pattern the file path pattern used to build include file paths.
  */
-function import_init(string $path): void
+function _init(string $path_pattern): void
 {
-    $GLOBALS['._pirogue.import.pattern'] = $path;
+    $GLOBALS['._pirogue.import.path_pattern'] = $path_pattern;
+}
+
+/**
+ * cleanup library resources.
+ *
+ * @uses $GLOBALS['._pirogue.import.path_pattern']
+ * @return void
+ */
+function _finalize(): void
+{
+    unset(
+        $GLOBALS['._pirogue.import.path_pattern']
+    );
 }
 
 /**
  * load an include file.
  *
- * @uses $GLOBALS['._pirogue.import.pattern']
+ * @uses $GLOBALS['._pirogue.import.path_pattern']
  *
  * @throws ErrorException the error thrown if the requested library file is not found.
  * @param string $name the name of library being loaded (translates to the filename without extension).
  */
-function import_load(string $name): void
+function import(string $name): void
 {
-    $include_file = sprintf($GLOBALS['._pirogue.import.pattern'], $name);
+    $include_file = sprintf($GLOBALS['._pirogue.import.path_pattern'], $name);
     if (false == file_exists($include_file)) {
         throw new ErrorException(sprintf('Unable to find library: %s (%s).', $name, $include_file));
     }
