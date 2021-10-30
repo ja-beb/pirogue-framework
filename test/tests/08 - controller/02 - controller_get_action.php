21fa5a454c18c10eaa574dbf4b71d14a6d7ef24b
Sean Bourg <sean.bourg@gmail.com>
2021-10-29 19:44:51 -0500

    updated controller test action test's expected results.

--- test/tests/08 - controller/02 - controller_get_action.php
+++ test/tests/08 - controller/02 - controller_get_action.php
@@ -25,13 +25,13 @@ pirogue_test_execute("controller_get_action(): testing_access_update_get", funct
 });
 
 pirogue_test_execute("controller_get_action(): testing_access_index_get", function () {
-    return _controller_get_action_test('index', 'GET', 'testing_access\index_get');
+    return _controller_get_action_test('index', 'GET', 'example_controller\index_get');
 });
 
 pirogue_test_execute("controller_get_action(): testing_access_index_post", function () {
-    return _controller_get_action_test('index', 'POST', 'testing_access\index_post');
+    return _controller_get_action_test('index', 'POST', 'example_controller\index_post');
 });
 
 pirogue_test_execute("controller_get_action(): testing_access_fallback_post", function () {
-    return _controller_get_action_test('fallback', 'POST', 'testing_access\fallback_get');
+    return _controller_get_action_test('fallback', 'POST', 'example_controller\fallback_get');
 });
