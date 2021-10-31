26d9f15012726728e38c8e07748e297c445171ea
Sean Bourg <sean.bourg@gmail.com>
2021-10-31 14:01:48 -0500

    added try/catch for connection testing.

--- test/tests/05 - mysqli/03 - _open.php
+++ test/tests/05 - mysqli/03 - _open.php
@@ -14,16 +14,28 @@ require_once(implode(DIRECTORY_SEPARATOR, [_PIROGUE_TESTING_PATH, 'include', 'pi
 // set up testing environment
 mysqli\_init(implode(DIRECTORY_SEPARATOR, [_PIROGUE_TESTING_PATH, 'config', 'mysqli-%s.ini']), 'website');
 
-pirogue_test_execute('_open(): invalid connection', fn () => null == mysqli\_open(
-    hostname: 'invalid-hostname',
-    username: 'invalid-user',
-) ? '' : 'able to open invalid connection');
-pirogue_test_execute('_open(): valid connection', fn () => null == mysqli\_open(
-    hostname: 'pirogue-database',
-    username: 'website-user',
-    password: 'website-password',
-    database: 'website',
-) ? 'able to open invalid connection' : '');
+pirogue_test_execute('_open(): invalid connection', function () {
+    try {
+        $connection = mysqli\_open(hostname: 'invalid-hostname', username: 'invalid-user');
+        return null == $connection ? '' : 'able to open invalid connection';
+    } catch (Throwable) {
+        return '';
+    }
+});
+
+pirogue_test_execute('_open(): valid connection', function () {
+    try {
+        $connection = mysqli\_open(
+            hostname: 'pirogue-database',
+            username: 'website-user',
+            password: 'website-password',
+            database: 'website',
+        );
+        return null == $connection ? 'unnable to open valid connection' : '';
+    } catch (Throwable) {
+        return 'unable to open valid connection';
+    }
+});
 
 // clean up testing environment
 mysqli\_dispose();
