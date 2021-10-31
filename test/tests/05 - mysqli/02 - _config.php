db0f20a006434921834316af32bb8b11f4dc32b8
Sean Bourg <sean.bourg@gmail.com>
2021-10-31 13:36:24 -0500

    added _config() testing.

---
+++ test/tests/05 - mysqli/02 - _config.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Testing \pirogue\database\mysqli\_config().
+ * php version 8.0.0
+ *
+ * @author Bourg, Sean <sean.bourg@gmail.com>
+ */
+
+use pirogue\database\mysqli;
+
+require_once(implode(DIRECTORY_SEPARATOR, [_PIROGUE_TESTING_PATH, 'include', 'pirogue', 'database', 'mysqli.php']));
+
+// set up testing environment
+mysqli\_init(implode(DIRECTORY_SEPARATOR, [_PIROGUE_TESTING_PATH, 'config', 'mysqli-%s.ini']), 'website');
+
+pirogue_test_execute('_config(): invalid label', fn()  => null == mysqli\_config('invalid-website') ? '' : 'loaded invalid connection.');
+pirogue_test_execute('_config(): valid label', fn () => _pirogue_test_mysqli_query(mysqli\get('website')));
+
+// clean up testing environment
+mysqli\_dispose();
