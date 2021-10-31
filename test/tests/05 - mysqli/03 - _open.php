e29dfd392bbe402637a558200b3a812aae08beaa
Sean Bourg <sean.bourg@gmail.com>
2021-10-31 13:52:37 -0500

    implemented test for _open() function.

--- test/tests/05 - mysqli/03 - _open.php
+++ test/tests/05 - mysqli/03 - _open.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Testing \pirogue\database\mysqli\get().
+ * Testing \pirogue\database\mysqli\_open().
  * php version 8.0.0
  *
  * @author Bourg, Sean <sean.bourg@gmail.com>
@@ -14,33 +14,16 @@ require_once(implode(DIRECTORY_SEPARATOR, [_PIROGUE_TESTING_PATH, 'include', 'pi
 // set up testing environment
 mysqli\_init(implode(DIRECTORY_SEPARATOR, [_PIROGUE_TESTING_PATH, 'config', 'mysqli-%s.ini']), 'website');
 
-function _pirogue_test_mysqli_query(?mysqli $database_connection)
-{
-    $_sql_results = mysqli_query($database_connection, 'SELECT COUNT(id) AS user_count FROM users ORDER BY username');
-    $_sql_data = mysqli_fetch_assoc($_sql_results);
-    mysqli_free_result($_sql_results);
-    return array_key_exists('user_count', $_sql_data) ? '' : 'unable to get count of website.users rows.';
-}
-
-pirogue_test_execute('get(): invalid label', function () {
-    try {
-        _pirogue_test_mysqli_query(mysqli\get('no-such-connection'));
-        return 'Invalid database connection returned.';
-    } catch (Throwable) {
-        return '';
-    }
-});
-
-pirogue_test_execute('get(): valid label', fn () => _pirogue_test_mysqli_query(mysqli\get('website')));
-pirogue_test_execute('get(): default label', fn () => _pirogue_test_mysqli_query(mysqli\get()));
-pirogue_test_execute('pirogue_get(): website-invalid', function () {
-    try {
-        _pirogue_test_mysqli_query(mysqli\get('website-invalid'));
-        return 'invalid database connection returned.';
-    } catch (Throwable) {
-        return '';
-    }
-});
+pirogue_test_execute('_open(): invalid connection', fn () => null == mysqli\_open(
+    hostname: 'invalid-hostname',
+    username: 'invalid-user',
+) ? '' : 'able to open invalid connection');
+pirogue_test_execute('_open(): valid connection', fn () => null == mysqli\_open(
+    hostname: 'pirogue-database',
+    username: 'website-user',
+    password: 'website-password',
+    database: 'website',
+) ? 'able to open invalid connection' : '');
 
 // clean up testing environment
 mysqli\_dispose();
