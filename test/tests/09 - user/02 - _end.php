9fe6c80ac63de77cf5511b5f0d5f0831d1245f48
Sean Bourg <sean.bourg@gmail.com>
2021-10-31 15:44:26 -0500

    Issue/34 (#45)
    * added session library.
    
    * restructured to add session library testing.
    
    * restructured to add session library testing.
    
    * corrected namespace.
    
    * added testing
    
    * removed session_write_close() call from libraries.

---
+++ test/tests/09 - user/02 - _end.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Testing pirogue\session\user\_end().
+ * php version 8.0.0
+ *
+ * @author Bourg, Sean <sean.bourg@gmail.com>
+ * @license https://opensource.org/licenses/GPL-3.0 GPL-v3
+ */
+
+use pirogue\session\user;
+
+require_once implode(DIRECTORY_SEPARATOR, [_PIROGUE_TESTING_PATH, 'include', 'pirogue', 'session', 'user.php']);
+
+$_SESSION = [];
+$GLOBALS['._pirogue-testing.session.user.label'] = '.testin-user-session';
+$GLOBALS['._pirogue-testing.session.user'] = ['id' => 1, 'name' => 'test user'];
+user\_init($GLOBALS['._pirogue-testing.session.user.label']);
+
+pirogue_test_execute('_end()', function () {
+    $_SESSION[$GLOBALS['._pirogue.session.user.label']] = $GLOBALS['._pirogue-testing.session.user'];
+    user\_end();
+    return array_key_exists($GLOBALS['._pirogue.session.user.label'], $_SESSION) ? 'session not cleared.' : '';
+});
+
+user\_dispose();
+unset($_SESSION);
