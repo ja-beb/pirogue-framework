2f2d55c2ba010c5e72cf7f92d2719ab9331231f1
Sean Bourg <sean.bourg@gmail.com>
2021-11-05 15:59:33 -0500

    update variable names

--- src/dispatcher/controller.php
+++ src/dispatcher/controller.php
@@ -12,50 +12,50 @@ namespace pirogue\dispatcher;
 /**
  * controller namespace
  * @internal
- * @var string $GLOBALS['._pirogue.controller_namespace']
+ * @var string $GLOBALS['._pirogue.dispatcher_controller_namespace']
  */
-$GLOBALS['._pirogue.controller_namespace'] = '';
+$GLOBALS['._pirogue.dispatcher_controller_namespace'] = '';
 
 /**
  * initialize library.
  * @internal
- * @uses $GLOBALS['._pirogue.controller_namespace']
+ * @uses $GLOBALS['._pirogue.dispatcher_controller_namespace']
  * @param string $name the name of the controller that is loaded.
  * @return void.
  */
 function _init(string $name) : void
 {
-    $GLOBALS['._pirogue.controller_namespace'] = $name;
+    $GLOBALS['._pirogue.dispatcher_controller_namespace'] = $name;
 }
 
 /**
  * clean up library variables.
  * @internal
- * @uses $GLOBALS['._pirogue.controller_namespace']
+ * @uses $GLOBALS['._pirogue.dispatcher_controller_namespace']
  * @return void
  */
 function _dispose() : void
 {
-    unset($GLOBALS['._pirogue.controller_namespace']);
+    unset($GLOBALS['._pirogue.dispatcher_controller_namespace']);
 }
 
 /**
  * determine if user has access.
- * @uses $GLOBALS['._pirogue.controller_namespace']
+ * @uses $GLOBALS['._pirogue.dispatcher_controller_namespace']
  * @param ?int $user_id the user id to check for access.
  * @return bool access flag.
  */
 function has_access(?int $user_id) : bool
 {
     return call_user_func(
-        sprintf('%s\has_access', $GLOBALS['._pirogue.controller_namespace']),
+        sprintf('%s\has_access', $GLOBALS['._pirogue.dispatcher_controller_namespace']),
         $user_id
     );
 }
 
 /**
  * execute controller action.
- * @uses $GLOBALS['._pirogue.controller_namespace']
+ * @uses $GLOBALS['._pirogue.dispatcher_controller_namespace']
  * @param string $action the contoller action to invoke.
  * @param array $request_path the request path to pass the action.
  * @param array $request_data the request data to pass the action.
@@ -65,7 +65,7 @@ function has_access(?int $user_id) : bool
 function exec(string $action, array $request_path, array $request_data, array $form_data = []) : array
 {
     return call_user_func(
-        sprintf('%s\%s', $GLOBALS['._pirogue.controller_namespace'], $action),
+        sprintf('%s\%s', $GLOBALS['._pirogue.dispatcher_controller_namespace'], $action),
         $request_path,
         $request_data,
         $form_data
