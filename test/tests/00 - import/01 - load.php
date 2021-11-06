f6f51b12c227edc8e37d1578d4546b8133dedb28
Sean Bourg <sean.bourg@gmail.com>
2021-11-06 14:34:29 -0500

    updated coments.

--- test/tests/00 - import/01 - load.php
+++ test/tests/00 - import/01 - load.php
@@ -14,7 +14,7 @@ use function pirogue\import_load;
 require_once implode(DIRECTORY_SEPARATOR, [_PIROGUE_TESTING_PATH, 'include', 'pirogue', 'import.php']);
 
 _import_init(implode(DIRECTORY_SEPARATOR, [_PIROGUE_TESTING_PATH, 'include', '%s.php']));
-pirogue_test_execute('import_load(): invalid file', function () {
+pirogue_test_execute('pirogue\import_load(): invalid file', function () {
     try {
         import_load('file-not-found');
         return 'Loaded invalid library file.';
@@ -23,5 +23,5 @@ pirogue_test_execute('import_load(): invalid file', function () {
     }
 });
 
-pirogue_test_execute('import_load(): valid file', fn() => import_load('pirogue/dispatcher'));
+pirogue_test_execute('pirogue\import_load(): valid file', fn() => import_load('pirogue/dispatcher'));
 _import_dispose();
